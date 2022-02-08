3aa6b862efee3468497b6d15ceb426ef8e4c9c58
Robin Burri <robinburri@tutanota.com>
2022-02-08 11:01:57 +0100

    Makefile + .h added

---
+++ philo/philo.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rburri <rburri@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/07 11:22:59 by rburri            #+#    #+#             */
+/*   Updated: 2022/02/08 11:00:05 by rburri           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <unistd.h>
+# include <pthread.h>
+
+
+#endif
